7cdc00f5a29af8da483aa15062a1f2ebf81420d8
Hang <git@hang.im>
2019-03-14 16:06:21 +0800

    reduce intermediate output

--- python/Main.py
+++ python/Main.py
@@ -10,6 +10,13 @@ TEMP_DIR = "/data/tmp"
 
 word_pattern = re.compile(r"[a-zA-Z0-9]+")
 alpha_list = "abcdedfhijklmnopqrstuvwxyzABCDEDFHIJKLMNOPQRSTUVWXYZ0123456789"
+TEMP_DICT_LIMIT = 10 ** 6  # for 32 GB, this should be 10 ** 7
+
+
+def dump_dict(tmp_dict, file):
+    print("dump tmp dict...")
+    for word, (index, count) in tmp_dict.items():
+        file.write("{},{},{}\n".format(word, index, count))
 
 
 def solve():
@@ -18,9 +25,12 @@ def solve():
     # open tmp files, total 62 files
     tmp_file = []
     for it in alpha_list:
-        temp_filename = "{}.tmp".format(it)
-        temp_filepath = os.path.join(TEMP_DIR, temp_filename)
-        tmp_file.append(open(temp_filepath, "w+"))
+        tmp_filename = "{}.tmp".format(it)
+        tmp_filepath = os.path.join(TEMP_DIR, tmp_filename)
+        tmp_file.append(open(tmp_filepath, "w+"))
+
+    # init tmp dict list, total 62 dicts
+    tmp_dict_list = [dict() for _ in range(len(alpha_list))]
 
     # step 1, read input file and split to 62 files, write (word, index, count=1)
     with open(INPUT_FILE, "r") as input_file:
@@ -32,11 +42,27 @@ def solve():
                 if not word or word[0] not in alpha_list or ',' in word:
                     continue
                 # print(word, index)  # debug
+
                 index += 1
-                tmp_file[alpha_list.find(word[0])].write("{},{},1\n".format(word, index))
+                tmp_dict = tmp_dict_list[alpha_list.find(word[0])]  # update tmp dict
+                if word not in tmp_dict:
+                    tmp_dict[word] = [index, 1]  # {word: [index, count=1]}
+                else:
+                    it = tmp_dict[word]  # {word: [index.min, count.sum]}
+                    it[0] = min(it[0], index)
+                    it[1] = it[1] + 1
+                if len(tmp_dict) > TEMP_DICT_LIMIT:  # memory limit for each dict
+                    dump_dict(tmp_dict, tmp_file[alpha_list.find(word[0])])
+                    tmp_dict.clear()
+
             line_index += 1
             if line_index % 100000 == 0:
                 print("process {} lines...".format(line_index))
+
+    # dump all tmp dict
+    for i in range(len(tmp_dict_list)):
+        dump_dict(tmp_dict_list[i], tmp_file[i])
+        tmp_dict_list[i].clear()
 
     # step 2, process each tmp file, (word, index, count) --> (word, index.min, count.sum)
     # step 3, filter count.sum == 1 and only save first word
